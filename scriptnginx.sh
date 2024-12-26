df7121ea7fcc09a52d28cc8e2acc5c3ac3eb577e
Marcele Montalvão <mmontalvaoblog@outlook.com>
2024-12-26 14:28:26 -0300

    Adicionado script de monitoramento do Nginx

---
+++ scriptnginx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+datahora=$(date +"%Y-%m-%d %H:%M:%S")
+servico="Nginx"
+
+if systemctl status nginx | grep "active (running)"; then
+  status="ONLINE"
+  mensagem="O serviço Nginx está online."
+  arquivo_saida="nginx_online.log"
+else
+  status="OFFLINE"
+  mensagem="O serviço Nginx está offline."
+  arquivo_saida="nginx_offline.log"
+fi
+
+echo "$datahora - $servico - $status - $mensagem" >> "$arquivo_saida"
+
+
